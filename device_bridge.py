cd71f17731ac9f4b530c76579aa925ed2080feb5
aaron <aaronxavie2399@gmail.com>
2026-03-04 19:16:59 +0530

    Add subprocess ADB integration and exception handling to Device class

--- device_bridge.py
+++ device_bridge.py
@@ -1,3 +1,5 @@
+import subprocess
+
 class DeviceBridge:
 
     def __init__(self,serial:str):
@@ -8,16 +10,58 @@ class DeviceBridge:
         self.connected=False
 
     def connect(self):
-        '''
-        connect to adb devices
-        '''
-        raise NotImplementedError("connect to adb not implemented")
+        result=subprocess.run(
+            ["adb","devices"],
+            capture_output=True,
+            text=True
+        )
+
+        if self.serial in result.stdout:
+            self.connected=True
+            print(f"{self.serial} is connected")
+        else:
+            self.connected=False
+            print(f"{self.serial} not connected")      
+            print(f"available devices: \n {result.stdout}")
+    def disconnect(self):
+        self.connected=False
+        print(f"{self.serial} is diconnected")
+
+    def get_status(self):
+        return "connected" if self.connected else "disconnected"  
+
+
+    def run_command(self,command):
+        if not self.connected:
+            raise RuntimeError(f"{self.serial} not connected try connect()")
+        try:
+            result = subprocess.run(
+                ["adb", "-s", self.serial] + command.split(),
+                capture_output=True,
+                text=True,
+                timeout=0.0001  # absurdly small, forces timeout
+            )
+            result.check_returncode()  # raises if adb errored
+            return result.stdout, result.stderr
+        except subprocess.TimeoutExpired:
+            return None, "Command Timeout out"  
+        except subprocess.CalledProcessError as e:
+            return None,f"adb error{e}"  
         
-    def get_logs(self):
-         '''
-         getting logs from device
-         '''
-         raise NotImplementedError("get logs is not implemeted")
+            
+d = DeviceBridge("emulator-5554")  # replace with your device ID from adb devices
+d.connect()
+output, error = d.run_command("shell getprop ro.build.fingerprin")
+print(f"Device model: {output} and {error}")
+
+
+
+
+
+
+        
+        
+    
     
 
 
