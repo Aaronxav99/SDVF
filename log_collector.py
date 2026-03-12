6c946b38ba4e5c769fd0e9d23d419c0cbc0d87d9
aaron <aaronxavie2399@gmail.com>
2026-03-12 20:32:10 +0530

    logcollector is refactored with context manager support

--- log_collector.py
+++ log_collector.py
@@ -4,19 +4,45 @@ import sys,os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 class LogCollector:
-    def __init__(self,serial):
+    def __init__(self,serial,filename=r"D:\upskilling\tasks\month_1\week_1\SDVF\logs\logcat.txt"):
         self.serial=serial
         self.process=None
+        self.filename=filename
+        self.log_file=None
 
 
-    def start(self,filename="logs/logcat.txt"):
+    def start(self):
+        self.log_file=open(self.filename,"w")
         self.process=subprocess.Popen(
             ["adb","-s",self.serial,"logcat"],
-            stdout=open(filename,"w"),
-            stderr=subprocess.PIPE,
+            stdout=self.log_file,
+            stderr=subprocess.PIPE,  #subprocess.PIPE means — "capture this stream and give it to me in Python so I can read
             text=True
-        )    
+        )   
+        print(f"logcat started->{self.filename}") 
 
     def stop(self):
         if self.process:
             self.process.terminate()
+            self.process=None
+            print(f"logcat is stopped")
+        if self.log_file:
+            self.log_file.close()    
+            self.log_file=None
+
+    #context manager
+    def __enter__(self):
+        self.start()
+        return self
+    def __exit__(self, exc_type, exc, tb):
+        self.stop()
+        return False   # dont supress the exceptions 
+    
+
+if __name__ == "__main__":
+    import time
+    with LogCollector("emulator-5554") as collector:
+        print("collecting logs for 5 seconds...")
+        time.sleep(5)
+    print("done — check logs/logcat.txt")
+            
